02d9eaf411a4eec364faa69deb9fa8214cd7bd9a
cezkot <robbielawa@gmail.com>
2025-11-16 05:18:20 +0100

    Fukcjonalnosc 1 i 3 z pliku "Java labolatorium 2"

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,3 @@
-
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,25 +7,61 @@ public class Game {
     private Scanner scanner = new Scanner(System.in);
     private Random random = new Random();
 
-    public Game(Player player){
+    public Game(Player player) {
         this.player = player;
     }
 
+    public void start(int gameType, int gameDiff) {
+        switch (gameType) {
+            case 1:
+                gameNormal(gameDiff);
+                break;
+            case 2:
+                gameReverse(gameDiff);
+                break;
+            case 3:
+                gameMixed(gameDiff);
+        }
+    }
 
-    public void start(){
-        System.out.println("Zgadnij liczbe od 0 -100");
+    //Przyjmuje poziom trudnosci i zwraca liczbe która jest koncem zakresu zgadywania
+    private int gameNumberRange(int gameDiff) {
+        int numberRange;
 
-        int number = random.nextInt(101);
+        switch (gameDiff) {
+            case 1:
+                numberRange = 100;
+                break;
+            case 2:
+                numberRange = 10000;
+                break;
+            case 3:
+                numberRange = 1000000;
+                break;
+            default:
+                numberRange = 100;
+                break;
+        }
+
+        return numberRange;
+    }
+
+    //Komputer losuje gracz zgaduje
+    private void gameNormal(int gameDiff) {
+
+        int numberRange = gameNumberRange(gameDiff);
+
+        System.out.println("Zgadnij liczbe od 0-" + numberRange);
+
+        int number = random.nextInt(numberRange + 1);
         int guess = -1;
         int attempts = 0;
 
-        while(number != guess)
-        {
+        while (number != guess) {
             System.out.println("Podaj liczbe: ");
             System.out.println(number);
             guess = scanner.nextInt();
-            attempts++
-            ;
+            attempts++;
             if (guess > number) {
                 System.out.println("Za duzo!");
             } else if (guess < number) {
@@ -39,5 +74,44 @@ public class Game {
         System.out.println("Udalo ci sie w " + attempts + " podejsciach");
         player.savePlayerScore(attempts);
         System.out.println("Najlepszy wynik: " + player.getBestScore());
+
+    }
+
+    //Gracz losuje komputer zgaduje
+    private void gameReverse(int gameDiff) {
+        int numberRange = gameNumberRange(gameDiff);
+        System.out.println("Podaj liczbe z zakresu 0-" + numberRange);
+        int number = -1;
+
+        while (number <= 0 || number >= numberRange) {
+            number = scanner.nextInt();
+            if (number < 0 || number > numberRange) {
+                System.out.println("Podana liczba jest poza zakresem");
+            }
+        }
+        int computerGuees = -1;
+        int bound = numberRange;
+        int origin = 0;
+        int attempts = 0;
+        while (computerGuees != number) {
+            computerGuees = random.nextInt(origin, bound + 1);
+            if (computerGuees > number) {
+                bound = computerGuees;
+                System.out.println("Komputer wybrał:" + computerGuees + " Jest to za duzo teraz szuka w zakresie: " + origin + "-" + bound);
+            } else if (computerGuees < number) {
+                origin = computerGuees;
+                System.out.println("Komputer wybrał:" + computerGuees + " Jest to za malo teraz szuka w zakresie: " + origin + "-" + bound);
+            }
+            attempts++;
+
+        }
+        System.out.println("Komputer zgadl w " + attempts + " probach");
+
+        //DODAĆ ZAPISYWANIE WYNIKOW
+
+    }
+
+    //Komputer losuje gracz i koputer zgaduja na zmiane
+    private void gameMixed(int gameDiff) {
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
 
+
 void main() {
 
     Scanner scanner = new Scanner(System.in);
 
+//inicjalizacja gracza
     System.out.println("Podaj nazwe");
     String nickname = scanner.nextLine();
-
     Player player = new Player(nickname);
 
-
-
+// Zmienne i obiekty związane z gra
     Game game = new Game(player);
-
     boolean gameFlag = true;
+    int gameType;
+    int gameDiff;
 
     while (gameFlag) {
 
@@ -24,7 +25,17 @@ void main() {
 
         switch (GameFlag) {
             case 1:
-                game.start();
+                System.out.println("Podaj rodzaj rozgrywki");
+                System.out.println("1: normalna");
+                System.out.println("2: odwrotna");
+                System.out.println("3: mieszana");
+                gameType = scanner.nextInt();
+                System.out.println("Podaj poziom trudnosci");
+                System.out.println("1: 0-100");
+                System.out.println("2: 0-10000");
+                System.out.println("3: 0-1000000");
+                gameDiff = scanner.nextInt();
+                game.start(gameType,gameDiff);
                 break;
             case 2:
                 player.resetPlayerScore();
